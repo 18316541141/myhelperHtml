3e478e4e06a757e7d32c9cb5a0d6165a63a99d26
林嘉健 <1320579501@qq.com>
2019-10-22 18:08:20 +0800

    asdasd

---
+++ h5/src/common/menus/system/GlobalVariableList.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <el-breadcrumb separator-class="el-icon-arrow-right">
+      <el-breadcrumb-item>系统管理</el-breadcrumb-item>
+      <el-breadcrumb-item>系统全局变量</el-breadcrumb-item>
+    </el-breadcrumb>
+    <br />
+    <el-form ref="form" v-bind:model="formData" label-width="80px">
+        <el-form-item :label="'变量'+(index+1)" v-for="(globalVariable,index) in formData.globalVariableList" :key="index">
+            
+        </el-form-item>
+    </el-form>
+  </div>
+</template>
+<script>
+export default {
+    data(){
+        return {
+            formData: {
+                globalVariableList:[{ varName: '', varValue: '' }]
+            }
+        };
+    }
+};
+</script>
