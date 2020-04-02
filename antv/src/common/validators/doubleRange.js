fa1b68ba601da114e1cc69b98467469f12d585ec
林嘉健 <1320579501@qq.com>
2020-04-02 14:20:52 +0800

    asdasd

--- antv/src/common/validators/doubleRange.js
+++ antv/src/common/validators/doubleRange.js
@@ -7,17 +7,16 @@
  */
 export default function (min, max, decimalCount, message) {
     return function (rule, value, callback) {
-        if (value === undefined || value === null || value === '') {
+        if (value === undefined || value === null || value === "") {
             callback();
         } else {
-            if(isNaN(value)){
-                callback(message.replace('{0}', min).replace('{1}', max));
-            } else{
-                if(decimalCount !== -1){
-                    var times = Math.pow(10,decimalCount);
-                    var ret = ((parseFloat(value)*times - parseInt(value)*times)/times + "").split(".");
-                    if(ret.length>1 && ret[1].length>decimalCount){
-                        callback(message.replace('{0}', min).replace('{1}', max));        
+            if (isNaN(value)) {
+                callback(message.replace("{0}", min).replace("{1}", max));
+            } else {
+                if (decimalCount !== -1) {
+                    var numStr = parseFloat(value).toString();
+                    if (numStr.indexOf('.') > -1 && numStr.length - numStr.indexOf('.') - 1 > decimalCount) {
+                        callback(message.replace("{0}", min).replace("{1}", max));
                     }
                 }
             }
@@ -25,8 +24,8 @@ export default function (min, max, decimalCount, message) {
             if (numVal >= min && numVal <= max) {
                 callback();
             } else {
-                callback(message.replace('{0}', min).replace('{1}', max));
+                callback(message.replace("{0}", min).replace("{1}", max));
             }
         }
-    }
+    };
 }
